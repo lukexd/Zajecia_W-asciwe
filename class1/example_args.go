8282af11bdcd6e29ccfa00bc1d4bef5cc11d7a67
Piotr Kołodyński <hehlolrofl@gmail.com>
2020-09-19 14:04:53 +0200

    branch 12

--- class1/example_args.go
+++ class1/example_args.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"math"
 	"strconv"
 )
 
@@ -23,6 +24,11 @@ func ControlProgramWithArgs() {
 		w := sumElements(flag.Args())
 		fmt.Println(w)
 	}
+
+	if *powerMode > 1 {
+		a := powerArg(flag.Args(), *powerMode)
+		fmt.Println(a)
+	}
 }
 
 func sumElements(elements []string) int {
@@ -39,4 +45,20 @@ func sumElements(elements []string) int {
 		sum += number
 	}
 	return sum
+}
+
+func powerArg(args []string, power int) int {
+	//Najpierw spr długość slica czy jest wiekszy od 0
+	if len(args) < 1 {
+		//log.Fatalf konczy działanie całego programu
+		log.Fatalf("Nie podałeś argumentu")
+	}
+	argument := args[0]
+	number, err := strconv.Atoi(argument)
+
+	if err != nil {
+		log.Fatalf("Nie udalo się zrzutować argumentu: %v na int", err)
+	}
+
+	return int(math.Pow(float64(number), float64(power)))
 }
